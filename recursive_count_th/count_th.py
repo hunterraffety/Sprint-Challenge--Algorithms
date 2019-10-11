85567f873d80418993bdc47ae8cf5692c45d5ac6
Hunter Raffety <hsraffety@gmail.com>
2019-10-11 11:49:46 -0700

    the tests are accumulating and trying to reset the list inside the base case just sets the overall list to 0 since inevitably its going to hit less than two letters. wont work.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,11 +7,13 @@ Your function must utilize recursion. It cannot contain any loops.
 cache = []
 print(f"cache length outside: {len(cache)}")
 def count_th(word):
+    global cache
     #count = 0
     #print(f"the word: {word}")
     #print(f"length: {len(word)}")
     if len(word) < 2:  #base case
         #print(f"not long enough")
+        #cache = [] cant reset here
         return
     elif word[0:2] == "th":  # needs to be elif
         # add word to cache here?
@@ -25,6 +27,11 @@ def count_th(word):
     #print(f"long enough")  #invoke recursion here
     count_th(leftover_word)  #generates infinite loop when a word longer than 2 is entered
         # lol fires off infinitely til: RecursionError: maximum recursion depth exceeded while calling a Python object
+
+
+        #return cache.count("th") won't pass tests either
+        #this doesn't make sense to store it as a dict as there needs to be no key:value relationship since everything in there is just going to be "th". my logic is failing here in that if the tests are all passing just like they're 
+    
     return len(cache)
     # TBC <-- what does this mean?
     # initial thoughts
@@ -36,6 +43,13 @@ def count_th(word):
 
 #print(count_th("")) #exit #### exits
 #print(count_th("thehehethht")) #this has 2 #### prints 2
-
-print(count_th("THththeeeee")) #this has 1 TH but only 2 lower #### prints 2
+#ok. stop and think. this is invoked and prints 3 times.
+#print(count_th("THththeeeee")) #this has 1 TH but only 2 lower #### prints 2
 #print(count_th("asdsfasghgasheeeee")) # this has 0 #### prints 0
+
+#print(count_th("abcthxyz"))
+#print(count_th("abcthefthghith"))
+#print(count_th("thhtthht"))
+print(count_th("THtHThth"))
+
+#tests are accumulating the list. 
