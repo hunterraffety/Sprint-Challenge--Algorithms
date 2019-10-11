65a3f87154c5969a284b97e45af2d8964325c470
Hunter Raffety <hsraffety@gmail.com>
2019-10-11 10:49:54 -0700

    testing recursion invokation

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,10 +4,23 @@ Your function should return a count of how many occurrences of ***"th"*** occur 
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+    count = 0
     print(f"the word: {word}")
+    print(f"length: {len(word)}")
+    if len(word) < 2:  #base case
+        print(f"not long enough")
+        return
+    else:
+        print(f"long enough")  #invoke recursion here
+        count_th(f"reinvokation of count_th: {word}") #generates infinite loop when a word longer than 2 is entered
+        pass
     # TBC <-- what does this mean?
+    # initial thoughts
+    # cant use a for loop
+    # 
     
     pass
 
+#print(count_th(""))
 print(count_th("thehehethht"))
-print(count_th("THththeeeee"))
+#print(count_th("THththeeeee"))
