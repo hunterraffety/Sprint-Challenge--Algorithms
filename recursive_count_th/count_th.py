483583d84c99278bb9ae2acf1b575cc021cb8dc3
Hunter Raffety <hsraffety@gmail.com>
2019-10-11 11:06:39 -0700

    else to elif to check for characters to trigger recursion

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,16 +8,18 @@ cache = []
 print(f"cache length outside: {len(cache)}")
 def count_th(word):
     #count = 0
-    print(f"the word: {word}")
-    print(f"length: {len(word)}")
+    #print(f"the word: {word}")
+    #print(f"length: {len(word)}")
     if len(word) < 2:  #base case
         print(f"not long enough")
         return
-    else:
+    elif word[0:2] == "th":  # needs to be elif
+        test_word = word[0:2]
+        print(f"test word: {test_word}")
+        print(f"word after check: {word} {len(word)}")
         print(f"cache length inside: {len(cache)}") # wont print under "" test case -- only when it passes
-        print(f"long enough")  #invoke recursion here
+        #print(f"long enough")  #invoke recursion here
         count_th(f"reinvokation of count_th: {word}") #generates infinite loop when a word longer than 2 is entered
-        pass
     # TBC <-- what does this mean?
     # initial thoughts
     # cant use a for loop
@@ -25,7 +27,8 @@ def count_th(word):
     # it will only look for "th" and ignore everything else out outside of the first two letters because of slice
     # i should use a cache list and append the found "th" in it instead of a count and just return the length of the cache len(cache)
     pass
-
-print(count_th(""))
-#print(count_th("thehehethht"))
+    
+#print(count_th(""))
+print(count_th("thehehethht"))
+#ok. stop and think. this is invoked and prints 3 times.
 #print(count_th("THththeeeee"))
